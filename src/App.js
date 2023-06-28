bd97b1f11f797df0a161423bcae0abbe83e97d7f
rajemurugesh <82995662+rajemurugesh@users.noreply.github.com>
2023-06-28 12:52:19 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useTranslation } from "react-i18next";
+
+// import Headerbar from './Components/Headerbar';
+// import Body from './Components/Body';
 
 function App() {
+  const { t, i18n } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{t("editLine")}</p>
+        <select onChange={(e) => i18n.changeLanguage(e.target.value)}>
+          <option>Choose Language</option>
+          <option value="ar">Arabic</option>
+          <option value="ru">Russian</option>
+          <option value="en">English</option>
+        </select>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t("welcome")}
+          <br />
+          {t("goodbye")}
         </a>
       </header>
     </div>
