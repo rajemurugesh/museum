6688b2c138e6b4073d7558b0de79bd67e1009c82
rajemurugesh <82995662+rajemurugesh@users.noreply.github.com>
2023-07-10 18:36:32 +0530

    search-function

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
+import React from 'react'
 import "./App.css";
-
-import { I18nextProvider } from "react-i18next";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
-import Header from "./Components/Header";
-import About from "./Components/About";
-import Contact from "./Components/Contact";
-import Home from "./Components/Home";
-import Error from "./Components/Error";
-import i18n from "./i18n";
-
+import Searchbar from './Components/Searchbar';
+import BookData from './mock-data.json';
 function App() {
-  return(
-    <I18nextProvider i18n={i18n}>
-    <Header />
-    <RouterProvider router={router}/>
-    
-    </I18nextProvider>
-  );
-  
+  return (
+    <div className='App'>
+      <Searchbar placeholder="Type Here to Search..." data={BookData}/>
+      </div>
+  )
 }
 
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route> 
-      <Route path="/" element={<Home />}/>
-      <Route path="/about" element={<About />}/>
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/error" element={<Error/>}/>
-
-    </Route>
-  )
-)
-
-
-
-
-
-export default App;
+export default App
