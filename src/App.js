7777ec9af9ffebec677cbc9914e3139319865e20
rajemurugesh <82995662+rajemurugesh@users.noreply.github.com>
2023-07-11 19:13:13 +0530

    form-notification

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
-import addNotification from 'react-push-notification';
+import React, { useState } from "react";
 import { Notifications } from 'react-push-notification';
+import addNotification from 'react-push-notification';
+
 function App() {
-function buttonOnClick (){
+const [name,setName] = useState("");
+
+function warningNotification (){
 	addNotification({
-	title: 'Success',
-	native:true		
+	title: 'Warning',
+	subtitle: 'Please fill it',
+	message: 'You have to enter name',
+	theme: 'red',
+	closeButton:"X",
 	})
 };
+
+function successNotification (){
+	addNotification({
+	title: 'Success',
+	subtitle: 'You have successfully submitted',
+	message: 'Welcome to Museum Application',
+	theme: 'light',
+	closeButton:"X",
+	backgroundTop:"green",
+	backgroundBottom:"yellowgreen"
+	})
+};
+
+function handleSubmit(e){
+	e.preventDefault();
+	if(name === "")
+	warningNotification();
+	else
+	successNotification()
+}
+
 return (
 	<div className="App">
 	<Notifications />
-	<h1>Hello World!</h1>
-	<button onClick={buttonOnClick}>
-		Push Notification
-	</button>
+	<h1>Guide App</h1>
+	<form >
+		<label>Name:</label>
+		<input name="name" value={name}
+		onChange={(e) => setName(e.target.value)}
+		/>
+		<button onClick={handleSubmit}
+		type="submit">Submit
+		</button>
+	</form>
 	</div>
 );
 }
